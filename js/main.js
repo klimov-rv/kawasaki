87a6c924f49fe5c1f18b56a08a0e00dbed9b1cb7
klimov-rv <Klimov.RV@yandex.ru>
2019-05-03 01:35:12 +0300

    menu offset fixed
    TNX GOD!

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 // Dropdown Menu Fade    
 jQuery(document).ready(function(){
+    
+    $(".dropdown-menu").css( "left", function( index ) {
+        return index = -$(".dropdown").offset().left;}
+         );
     $(".dropdown").hover(
         function() { $('.dropdown-menu', this).fadeIn("fast");
         },
