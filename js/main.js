4b8e02a10a737e45524b2b3d778a39ab062e3f3f
klimov-rv <Klimov.RV@yandex.ru>
2019-05-05 15:40:27 +0300

    Layout #2
    1. Главная
    2. Карточка товара
    3. Каталог плиткой

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-// Dropdown Menu Fade    
+  
 jQuery(document).ready(function(){
     
+    // Dropdown Menu   
     $(".dropdown-menu").css( "left", function( index ) {
         return index = -$(".dropdown").offset().left;}
          );
@@ -9,4 +10,12 @@ jQuery(document).ready(function(){
         },
         function() { $('.dropdown-menu', this).fadeOut("fast");
     });
+
+    // Tabs  
+    $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+        $(this)
+          .addClass('active').siblings().removeClass('active')
+          .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+      });
+
 });
